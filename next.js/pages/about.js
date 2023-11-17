d8e770315f5c963dd11eb252eed2e2f9469503ad
NAMEDgun <myeonggeon88@gmail.com>
2023-11-17 12:00:11 +0900

    Init: First NextJS Study Settings
    - By NomadCoder

---
+++ next.js/pages/about.js
@@ -0,0 +1,10 @@
+import NavBar from "../components/NavBar";
+
+export default function Potata() {
+    return (
+        <div>
+            <NavBar />
+            <h1>About Us</h1>
+        </div>
+    );
+}
