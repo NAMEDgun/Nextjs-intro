6d34be32b1e90e0fb00a493a79d02b8d26ed447a
NAMEDgun <myeonggeon88@gmail.com>
2023-11-17 14:22:38 +0900

    Feat: Studying Navigatino Bar

--- next.js/components/NavBar.js
+++ next.js/components/NavBar.js
@@ -1,14 +1,21 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "./NavBar.module.css";
 
 export default function NavBar() {
-    return <nav>
-        {/* <a href="/">Home</a>
-        <a href="/about">About</a> */}
-        <Link href="/">
-            <a>Home</a>
-        </Link>
-        <Link href="/about">
-            <a>About Us</a>
-        </Link>
-    </nav>
+    const router = useRouter();
+    return (
+        <nav className={styles.nav}>
+            {/* <a href="/">Home</a>
+            <a href="/about">About</a> */}
+            <Link href="/" legacyBehavior>
+                <a className={router.pathname === "/" ? styles.active : ""}>Home</a>
+                {/* <a style={{color: router.pathname === "/"? "red" : "blue"}}>Home</a> */}
+            </Link>
+            <Link href="/about" legacyBehavior>                
+                <a className={router.pathname === "/about" ? styles.active : ""}>About Us</a>
+                {/* <a style={{color: router.pathname === "/about"? "red" : "blue"}}>About Us</a> */}
+            </Link>
+        </nav>
+    );
 }
