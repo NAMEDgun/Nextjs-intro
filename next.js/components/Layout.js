7d110caca1a0f428117d902df1c0c8eaaf0be5d2
NAMEDgun <myeonggeon88@gmail.com>
2023-11-21 14:27:08 +0900

    Minor: Description of the code
    Parents&Children about the layout and componenets
    Dynamic methods of the page title

--- next.js/components/Layout.js
+++ next.js/components/Layout.js
@@ -1,10 +1,15 @@
 import NavBar from "./NavBar";
 
 export default function Layout({ children }) {
-    return <>
-        <NavBar />
-        <div>
-            {children}
-        </div>
-    </>
+    return (
+        // Fragment를 사용하여 여러 요소를 묶음
+        <>
+            {/* NavBar 컴포넌트를 렌더링 */}
+            <NavBar />
+            {/* children prop으로 전달된 내용을 감싼 div를 렌더링 */}
+            <div>
+                {children}
+            </div>
+        </>
+    );
 }
--- next.js/components/Lee.js
+++ next.js/components/Lee.js
@@ -1,8 +1,10 @@
 import Head from "next/head"
 
-export default function Lee({title}){
+export default function Lee({ title }) {
     return (
+        // HTML의 <head> 요소를 동적으로 설정하기 위해 Head 컴포넌트 사용
         <Head>
+            {/* 페이지의 제목을 동적으로 설정, "Next Movies"는 고정된 부분 */}
             <title>{title} | Next Movies</title>
         </Head>
     );
