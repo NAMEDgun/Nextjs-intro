e9b5fbe94a222cc877e7f2336da28e9db370a72f
MyeongGeon <myeonggeon88@gmail.com>
2024-03-06 18:16:16 +0900

    first commit message

---
+++ Desktop/Studying/Next.js/next.js/pages/about.js
@@ -0,0 +1,10 @@
+import Lee from "../components/Lee";
+
+export default function Potata() {
+    return (
+        <div>
+            <Lee title="About" />
+            <h1>About</h1>
+        </div>
+    );
+}
